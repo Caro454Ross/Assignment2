17859274b0a2618f127321e7351b539da5a54b9b
Caro454Ross <caro454rossj@gmail.com>
2014-04-14 21:56:28 +0200

    Updated

--- DNA_main.py
+++ DNA_main.py
@@ -9,9 +9,7 @@ seq_info=DNA_methods.read_fasta_file(fasta_file)
 print "The reverse complement of the sequence is:\n"+DNA_methods.reverse_complement(seq_info[1])
 
 ORF= DNA_methods.get_ORF(seq_info[1]) #returns ORF
-print ORF
 gene= DNA_methods.get_gene_by_ORF(seq_info[1],ORF)# gets gene from the determined ORF
-print gene
 protein=DNA_methods.translate(gene)#translates the gene into amino acid sequence
 fasta_protein=DNA_methods.get_fasta(seq_info[0]+' protein sequence',protein)
 
